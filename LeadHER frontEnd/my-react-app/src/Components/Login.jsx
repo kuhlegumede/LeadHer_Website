7bd8802aa60c3e484e3fb33094bb65df3fa2f2d0
Kuhle Gumede <112685568+kuhlegumede@users.noreply.github.com>
2026-08-25 15:59:48 +0200

    Update login API URL to use API_URL variable

--- LeadHER frontEnd/my-react-app/src/Components/Login.jsx
+++ LeadHER frontEnd/my-react-app/src/Components/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
     setSuccessMessage("");
 
     try {
-      const res = await fetch("https://localhost:7033/api/Auth/login", {
+      const res = await fetch("${API_URL}/api/Auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
